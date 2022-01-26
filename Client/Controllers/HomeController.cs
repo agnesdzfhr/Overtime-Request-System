5043c232aff3bba6f0c4f13096fa35d6b81bba2b
Agnes Diza Fahira <agnesdizaf@gmail.com>
2022-01-26 11:25:43 +0700

    Add Admin Layout

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Client.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
