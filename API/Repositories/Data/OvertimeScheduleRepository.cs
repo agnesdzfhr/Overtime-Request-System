7e542ce2089020c8764f3862856f4add38d05160
Agnes Diza Fahira <agnesdizaf@gmail.com>
2022-01-28 18:15:50 +0700

    Add GetForManager2

--- API/Repositories/Data/OvertimeScheduleRepository.cs
+++ API/Repositories/Data/OvertimeScheduleRepository.cs
@@ -32,35 +32,73 @@ namespace API.Repositories.Data
             myContext.OvertimesSchedules.AddRange(os);
             myContext.SaveChanges();
 
-
-            
             if (findManager != null)
             {
                 var findManagerAcc = myContext.Accounts.Where(a => a.NIK == findManager.NIK).FirstOrDefault();
-                
-                var fromAddress = new MailAddress("mcc.61.2021@gmail.com");
-                var passwordFrom = "kahardian61";
-                var toAddress = new MailAddress(findManagerAcc.Email);
-                SmtpClient smtp = new SmtpClient
-                {
-                    Host = "smtp.gmail.com",
-                    Port = 587,
-                    EnableSsl = true,
-                    UseDefaultCredentials = false,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    Credentials = new NetworkCredential(fromAddress.Address, passwordFrom)
-                };
-                using (var message = new MailMessage(fromAddress, toAddress)
-                {
-                    Subject = "Overtime Request",
-                    Body = "Hai",
-                    IsBodyHtml = true,
-                }) smtp.Send(message);
+
+                var toEmail = findManagerAcc.Email;
+                var subjectEmail = "Overtime Request";
+                var bodyEmail = "Hai, Ini hanya email uji coba";
+
+                SendEmail(toEmail, subjectEmail, bodyEmail);
+
                 return HttpStatusCode.OK;
             }
             else
             {
                 return HttpStatusCode.NotFound;
+            }
+        }
+
+        public IEnumerable<Object> GetForManager(string NIK)
+        {
+            //var findManager = myContext.Employees.Where( m => m.NIK == NIK).FirstOrDefault();
+            var findListEmployee = myContext.OvertimesSchedules.Include(os => os.Employee).Where(e => e.Employee.Manager_ID == NIK).ToList();
+
+            var listResult = new List<OvertimeSchedulesVM>();
+            foreach (var item in findListEmployee)
+            {
+                var result = new OvertimeSchedulesVM
+                {
+                    NIK = item.NIK,
+                    FullName = $"{item.Employee.FirstName} {item.Employee.LastName}",
+                    Date = item.Date,
+                    StartTime = item.StartTime,
+                    EndTime = item.EndTime,
+                    Note = item.Note
+
+                };
+
+                listResult.Add(result);
+            }
+
+            return listResult;
+        }
+
+        public static void SendEmail(string toEmail, string subjectEmail, string bodyEmail)
+        {
+            string to = toEmail;
+            string from = "mccreg61net@gmail.com";
+
+            MailMessage message = new MailMessage(from, to);
+
+            message.Subject = subjectEmail;
+            message.Body = bodyEmail;
+            message.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(from, "61mccregnet"),
+                EnableSsl = true
+            };
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
         }
     }
